b4c66ba742550f4f162848d1a823db9b294225aa
AndreaRootman <Rootmana2@middleton.school.nz>
2023-05-21 18:57:23 +1200

    2

--- Assessment_2.py
+++ Assessment_2.py
@@ -1,31 +1,3 @@
-"""Component 1 - welcome screen and instructions"""
-
-def get_name():
-    name_local = input("what is your name: ")
-    return name_local
-
-def get_age():
-    age_local = int(input("How old are you: "))
-    return age_local
-
-name = get_name()
-age = get_age()
-print(f"\nHello {name}. At {age} years old you may or may not find this quiz a bit easy.\n")
-
-show_instructions = input("Have you played this game before? :")
-
-if show_instructions == "yes" or show_instructions == "y" or show_instructions == "y" or show_instructions == "Yes":
- print("program continues")
-
-elif show_instructions == "no" or show_instructions == "n" or show_instructions == "No":
-    print("Display instructions")
-    print(f"\nSo {name}, in this quiz I will give you a word in Te Reo Maori.\n"
-          f"\n You must answer the question only in number form, good luck.\n")
-
-else:
-    print("please answer 'yes or 'no' ")
-
-print(f"entered '{show_instructions}' ")
 
 
 """component 2- Amount of questions the player wants"""
